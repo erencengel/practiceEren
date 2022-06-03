2b1d3079a613f4d99809df64b1114edee13219bf
eren.cengel <eren.cengel@scalefocus.com>
2022-06-03 16:06:36 +0300

    some changes has been added

---
+++ src/test/java/com/hepsiburada/tests/day9_popupAndAlertsandFrames/multipleWindows.java
@@ -0,0 +1,52 @@
+package com.hepsiburada.tests.day9_popupAndAlertsandFrames;
+
+import com.hepsiburada.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+
+public class multipleWindows {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver("chrome");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void test1(){
+        driver.get("http://practice.cybertekschool.com/windows");
+        System.out.println("title before switch = " + driver.getTitle());
+        String oldWindow = driver.getWindowHandle();
+        driver.findElement(By.xpath("//*[.='Opening a new window']/../a")).click();
+        Set<String> windowHandles = driver.getWindowHandles();
+        for (String windowHandle : windowHandles) {
+            if(!windowHandle.equals(oldWindow)) driver.switchTo().window(windowHandle);
+        }
+        Assert.assertTrue(driver.findElement(By.xpath("//h3[contains(text(),'New')]")).getText().equals("New Window"));
+    }
+
+    @Test
+    public void test2(){
+        driver.get("http://practice.cybertekschool.com/windows");
+        driver.findElement(By.xpath("//*[.='Opening a new window']/../a")).click();
+        Set<String> windowHandles = driver.getWindowHandles();
+        for (String windowHandle : windowHandles) {
+            driver.switchTo().window(windowHandle);
+            if(driver.getTitle().equals("New Window")) break;
+        }
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+    }
+
+}
--- src/test/java/com/hepsiburada/tests/day9_popupAndAlertsandFrames/popUpsAndAlerts.java
+++ src/test/java/com/hepsiburada/tests/day9_popupAndAlertsandFrames/popUpsAndAlerts.java
@@ -1,4 +1,32 @@
 package com.hepsiburada.tests.day9_popupAndAlertsandFrames;
 
+import com.hepsiburada.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class popUpsAndAlerts {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver("chrome");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void test1() throws InterruptedException {
+        driver.get("http://practice.cybertekschool.com/javascript_alerts");
+        driver.manage().window().maximize();
+        driver.findElement(By.cssSelector("button[onclick$='lert()']")).click();
+        Thread.sleep(2000);
+        driver.switchTo().alert().accept();
+    }
 }
