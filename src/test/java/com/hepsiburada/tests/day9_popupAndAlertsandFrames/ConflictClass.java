bfc79cbb5562d4215b49d360ade6f77aba2b3355
eren.cengel <eren.cengel@scalefocus.com>
2022-06-03 16:12:08 +0300

    asflasşdl

--- src/test/java/com/hepsiburada/tests/day9_popupAndAlertsandFrames/ConflictClass.java
+++ src/test/java/com/hepsiburada/tests/day9_popupAndAlertsandFrames/ConflictClass.java
@@ -2,6 +2,6 @@ package com.hepsiburada.tests.day9_popupAndAlertsandFrames;
 
 public class ConflictClass {
 
-    //this class has been created for practicing merge conflicts
+    //this class has been created for practicing merge...
 
 }
