0a840bc5280674e885d50829429d356571663cb4
eren.cengel <eren.cengel@scalefocus.com>
2022-06-06 09:11:03 +0300

    added js codes

---
+++ src/test/java/com/hepsiburada/tests/day10_actionsJs/JavaScriptExecutorDemo.java
@@ -0,0 +1,62 @@
+package com.hepsiburada.tests.day10_actionsJs;
+
+import com.hepsiburada.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class JavaScriptExecutorDemo {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.quit();
+    }
+
+    @Test
+    public void clickWithJS1(){
+        driver.get("http://practice.cybertekschool.com/");
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
+        javascriptExecutor.executeScript("arguments[0].click(0)",driver.findElement(By.xpath("//a[.='Dropdown']")));
+    }
+
+    @Test
+    public void clickWithJS2(){
+        driver.get("http://practice.cybertekschool.com/");
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
+        driver.findElement(By.xpath("//a[.='Web Tables']")).click();
+        //javascriptExecutor.executeScript("arguments[0].click(0)",driver.findElement(By.xpath("//a[.='Web Tables']")));
+    }
+
+    @Test
+    public void sendTextToDisabledButton(){
+        driver.get("http://practice.cybertekschool.com/dynamic_controls");
+        WebElement webElement = driver.findElement(By.cssSelector("form#input-example>input"));
+        String text = "Hello disabled button";
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("arguments[0].setAttribute('value', '" + text + "')", webElement);
+    }
+
+    @Test
+    public void scrollUpAndDown() throws InterruptedException {
+        driver.get("http://practice.cybertekschool.com/infinite_scroll");
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        for (int i = 0; i < 5; i++) {
+            jse.executeScript("window.scrollBy(0,20)");
+            Thread.sleep(1000);
+        }
+    }
+
+}
